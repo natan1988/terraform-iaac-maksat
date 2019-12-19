c63d1b1c4055bbe7eb06668f3d5f92d3a645008b
natan1988 <natan88@mail.ru>
2019-12-18 19:10:17 -0600

    fixed key name

--- output.tf
+++ output.tf
@@ -2,12 +2,12 @@ output "bucket_name" {
     value = aws_s3_bucket.b.bucket
 }
 
-output "secs_group" {
+output "sec_group" {
     value = aws_security_group.allow.SSH.name
 }
 
 
-output "sec_group" {
+output "key_name" {
     value = aws__key_pair.deployer.key_name
 }
 
