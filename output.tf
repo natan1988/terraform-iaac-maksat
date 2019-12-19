7da4a96e95c4d3c1d07e5c315e58238a71abb305
natan1988 <natan88@mail.ru>
2019-12-18 19:07:15 -0600

    output

--- output.tf
+++ output.tf
@@ -2,3 +2,12 @@ output "bucket_name" {
     value = aws_s3_bucket.b.bucket
 }
 
+output "sec_group" {
+    value = aws_security_group.allow.SSH.name
+}
+
+
+output "sec_group" {
+    value = aws__key_pair.allow.deployer.key_name
+}
+
