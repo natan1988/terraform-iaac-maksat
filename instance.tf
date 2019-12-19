7dcc7eff130ae90e4d568ca9082f455377789200
natan1988 <natan88@mail.ru>
2019-12-18 18:27:13 -0600

    FIXED NAME

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name= aws_key_pair.deployer.key_name
-   security_groups = "allow_ssh" 
+  security_groups = "allow_SSH" 
  user_data = file("userdata_file") 
 tags = { 
     Name = "HelloWorld" 
