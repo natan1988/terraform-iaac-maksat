e4f24207f5fdb48a1e4b7b20a077d61d9969bac5
natan1988 <natan88@mail.ru>
2020-01-05 16:20:45 -0600

    added count

--- instance.tf
+++ instance.tf
@@ -1,13 +1,17 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
+  count = 10
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name= aws_key_pair.deployer.key_name
   security_groups = ["allow_SSH"] 
  user_data = file("userdata_file") 
 tags = { 
-    Name = "HelloWorld" 
+    Name = "HelloWorld${count_index +1}" 
   } 
 } 
+
+
+
 
 
