94193c8a57ed921d70f25c1cbf58036c7d3cc2d9
natan1988 <natan88@mail.ru>
2020-01-05 17:17:55 -0600

    removed imported

--- instance.tf
+++ instance.tf
@@ -12,14 +12,10 @@ tags = {
 } 
 
 
-resource "aws_instance" "imported" {
-  ami           = "ami-00068cd7555f543d5"
-   key_name= aws_key_pair.deployer.key_name
-   security_groups = ["allow_SSH"]
-   instance_type = "t2.micro"
 
 
-}
+
+
 
 
 
