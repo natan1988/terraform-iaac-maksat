b6e42da3fa1d480f208b465b3dfc8d70ddb7a129
natan1988 <natan88@mail.ru>
2020-01-05 17:05:48 -0600

    added tfvars and variables

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" { 
   ami           = var.ami 
-  count = 10
-  instance_type = "t2.micro" 
+  count         = var.count
+  instance_type = var.instance_type
   associate_public_ip_address = "true" 
   key_name= aws_key_pair.deployer.key_name
   security_groups = ["allow_SSH"] 
