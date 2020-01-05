3a55048c13ef0f2abf7285e7c8389cf23066029c
natan1988 <natan88@mail.ru>
2020-01-05 17:08:25 -0600

    fixed count

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" { 
   ami           = var.ami 
-  count         = var.count
+  count         = var.count_instance
   instance_type = var.instance_type
   associate_public_ip_address = "true" 
   key_name= aws_key_pair.deployer.key_name
