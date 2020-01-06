601907d298e80900268548ac4d196c5b74700866
natan1988 <natan88@mail.ru>
2020-01-05 18:30:00 -0600

    sss

--- instance.tf
+++ instance.tf
@@ -6,11 +6,12 @@ resource "aws_instance" "web" {
   key_name= aws_key_pair.deployer.key_name
   security_groups = ["allow_SSH"] 
  
-   provisioner "remote-exec" {
+ 
+ provisioner "remote-exec" {
      connection { 
-       host        = self.public_ip
-       type        = "ssh" 
-       user        = var.user
+       host       = self.public_ip
+       type       = "ssh" 
+       user       = var.user
        private_key = file(var.ssh_key_location)
        } 
        inline = [
