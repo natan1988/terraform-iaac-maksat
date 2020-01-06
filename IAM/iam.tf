752a4bc9422536dc694bb0d3f5901fcacb18ae3f
natan1988 <natan88@mail.ru>
2020-01-05 21:08:57 -0600

    fixed

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -22,16 +22,16 @@ resource "aws_iam_policy" "policy" {
   path = "/" 
   description = "My test policy" 
   policy = <<EOF 
-{ 
-"Version": "2012-10-17", 
-"Statement": [ 
-{ 
-"Sid": "VisualEditor0", 
-"Effect": "Allow", 
-"Action": "ec2:*", 
-"Resource": "*" 
-} 
-] 
-} 
-EOF 
-} 
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Sid": "VisualEditor0",
+"Effect": "Allow",
+"Action": "ec2:*",
+"Resource": "*"
+}
+]
+}
+EOF
+}
