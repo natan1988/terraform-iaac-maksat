e48e1bc2c5f5d933292ae51c06f1fb90ecc607d4
natan1988 <natan88@mail.ru>
2020-01-05 21:15:24 -0600

    added role

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -35,3 +35,21 @@ resource "aws_iam_policy" "policy" {
 }
 EOF
 }
+resource "aws_iam_role" "test_role" {
+name = "test_role"
+assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+{
+  "Action": "sts:AssumeRole",
+  "Principal": {
+  "Service": "ec2.amazonaws.com"
+},
+  "Effect": "Allow",
+  "Sid": ""
+    }
+  ]
+}
+EOF
+}
