a9b2fa885f10eaf3678af72eb8dcc362683513cd
natan1988 <natan88@mail.ru>
2020-01-05 19:43:09 -0600

    added developers team

--- iam.tf
+++ iam.tf
@@ -10,7 +10,7 @@ resource "aws_iam_group" "developers" {
 } 
 
 
-resource "aws_iam_group_membership" "developers" { 
+resource "aws_iam_group_membership" "developers_team" { 
   name = "developers-group-membership" 
   users = [ 
     aws_iam_user.tim.name 
