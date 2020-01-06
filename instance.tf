82e9decf28e48c565e56592343627332d8f574ec
natan1988 <natan88@mail.ru>
2020-01-05 18:50:54 -0600

    added local exec

--- instance.tf
+++ instance.tf
@@ -19,7 +19,16 @@ resource "aws_instance" "web" {
         "sudo yum install httpd -y",
         "sudo systemctl start httpd",
         ] 
-       } 
+       }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web.public_ip} >> public_ips.txt"
+    command = " wget -O /tmp https://wordpress.org/latest.zip"
+    command = "mkdir /tmp/test"
+  }
+  lifecycle{
+    prevent_destroy = false
+  }
+
 tags = { 
     Name = "HelloWorld${count.index +1}" 
   } 
