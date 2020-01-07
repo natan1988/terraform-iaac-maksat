b40ff9f0822900af9e38d57d6a92f3460a424b46
natan1988 <natan88@mail.ru>
2020-01-06 20:51:35 -0600

    ADDED

--- application/infrastructure.tf
+++ application/infrastructure.tf
@@ -4,7 +4,7 @@ module "wordpress1" {
   name = var.name
   cidr = var.cidr
 
-  azs             = [var.azs1 , var.azs2, var.azs3]
+  azs             = [var.azs1, var.azs2, var.azs3]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
