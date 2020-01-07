c3d90d08feb107d5894d8283654c87889f7164e6
natan1988 <natan88@mail.ru>
2020-01-06 20:36:37 -0600

    added

--- application/infrastructure.tf
+++ application/infrastructure.tf
@@ -4,7 +4,7 @@ module "wordpress1" {
   name = var.name
   cidr = var.cidr
 
-  azs             = ["eu-eaast-1a", "eu-east-1b", "eu-east-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
