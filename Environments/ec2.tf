1801e444525e780ef00881846ce700a589271077
natan1988 <natan88@mail.ru>
2020-01-07 20:36:13 -0600

    tags

--- Environments/ec2.tf
+++ Environments/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instancee" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-    taqs = {
+    tags = {
         Name = var.environment
     }
   
