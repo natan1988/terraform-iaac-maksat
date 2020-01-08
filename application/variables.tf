08b7191efca85d81b4a5b7461f718325b7456c9c
natan1988 <natan88@mail.ru>
2020-01-08 14:20:25 -0600

    fixed variables

--- application/variables.tf
+++ application/variables.tf
@@ -6,7 +6,7 @@ variable "azs3" {}
 variable "private_subnets1" {}
 variable "private_subnets2" {}
 variable "private_subnets3" {}
-variable "pulic_subnets1" {}
-variable "pulic_subnets2" {}
-variable "pulic_subnets3" {}
+variable "public_subnets1" {}
+variable "public_subnets2" {}
+variable "public_subnets3" {}
 
