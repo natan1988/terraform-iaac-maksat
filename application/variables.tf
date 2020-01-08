03f80cfcfbc857787a40a61f382958f353d3bc17
natan1988 <natan88@mail.ru>
2020-01-08 14:02:30 -0600

    added subents variables

--- application/variables.tf
+++ application/variables.tf
@@ -3,4 +3,10 @@ variable "cidr" {}
 variable "azs1" {}
 variable "azs2" {}
 variable "azs3" {}
+variable "private_subnets1" {}
+variable "private_subnets2" {}
+variable "private_subnets3" {}
+variable "pulic_subnets1" {}
+variable "pulic_subnets2" {}
+variable "pulic_subnets3" {}
 
