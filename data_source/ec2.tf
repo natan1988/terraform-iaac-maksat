4486e0760ad9d65fa9adc2d312cfab7b97948e95
natan1988 <natan88@mail.ru>
2020-01-08 18:39:59 -0600

    removed spaces

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2"
 }
 
 
@@ -9,13 +9,13 @@ provider "aws" {
 
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
+  most_recent = true
+  owners = ["099720109477"]
 }
 
 
 
 
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}"
+  value = "${data.aws_ami.ubuntu.id}"
 }
