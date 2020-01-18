238eff70d1b21c03245d21b62b1c5efcf66bdf12
natan1988 <natan88@mail.ru>
2020-01-18 12:28:58 -0600

    added azure vm

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -108,3 +108,83 @@ resource "azurerm_virtual_machine" "vm1" {
     environment = "staging"
   }
 }
+
+
+resource "azurerm_network_interface" "nic2" { 
+
+  name = "nic2" 
+
+  location = "westus2" 
+
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+
+  ip_configuration { 
+
+    name = "testconfiguration2" 
+
+    subnet_id = "${azurerm_subnet.private.id}" 
+
+    private_ip_address_allocation = "Dynamic" 
+
+    public_ip_address_id = "${azurerm_public_ip.IP2.id}" 
+
+  } 
+
+} 
+
+ 
+
+ 
+
+resource "azurerm_public_ip" "IP2" { 
+
+  name = "public_ip2" 
+
+  location = "westus2" 
+
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+
+  allocation_method = "Dynamic" 
+
+} 
+
+ 
+
+ 
+
+ 
+
+resource "azurerm_virtual_machine" "vm2" {
+  name = "vm2"
+  location = "westus2"
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic2.id}"]
+  vm_size = "Standard_DS1_v2"
+  storage_image_reference {
+  publisher = "OpenLogic"
+  offer = "CentOS"
+  sku = "7.5"
+  version = "latest"
+}
+
+storage_os_disk {
+  name = "myosdisk2"
+  caching = "ReadWrite"
+  create_option = "FromImage"
+  managed_disk_type = "Standard_LRS"
+}
+os_profile {
+  computer_name = "vm2"
+  admin_username = "centos"
+}
+os_profile_linux_config {
+  disable_password_authentication = true
+  ssh_keys {
+  path = "/home/centos/.ssh/authorized_keys"
+  key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGCdXIBM6eKpYJv0EuZlZx1FQi4UAvqonQMdyik/dCUTAGPK5Wel6Xl0rTa4tuPKHYeEMVCetit4MAIwJRT8WTsJsr14H46wkonEQdnC7fXBKGKF390WR7hxEHh7SnwSJLwnXcywoi35VhFX98VXTc27ez7lta6XGZ7FF4cxBJdar66KSdpOFJ9392ni1mEIhXzeKaYNAb2BI2CYhh2uKRp5abpdvFPJcue0geTgf92AXg+nu6KyHcoFXcVWmJE8ZhFlXeQg/Ey0EduRqkbDEBoj6VVbMvnjpSVLT6qXQJASLm8KzRYPxTXellQkZbnyBIYBfOz5RReJCk7i+MoT6n root@ip-172-31-1-248.eu-west-1.compute.internal"
+  }
+}
+tags {
+   environment = "staging"
+  }
+}
