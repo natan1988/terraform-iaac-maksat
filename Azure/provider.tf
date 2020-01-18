f4c5f9ecd55a08be1ed966fb805d38bd9c376af0
natan1988 <natan88@mail.ru>
2020-01-18 11:54:53 -0600

    added subnets

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -18,3 +18,13 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
   address_space = ["10.0.0.0/16"]
 }
+
+
+
+
+resource "azurerm_subnet" "private" {
+  name = "private"
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix = "10.0.1.0/24"
+}
