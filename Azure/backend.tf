c1ab5665845fb776d90d95934392601e131ae012
natan1988 <natan88@mail.ru>
2020-01-18 12:49:08 -0600

    added backend

---
+++ Azure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "example1"
+    storage_account_name = "example1maksat"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
