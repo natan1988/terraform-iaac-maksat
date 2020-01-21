dbf35611c365e1dcab655389f42e03525a5f0f12
natan1988 <natan88@mail.ru>
2020-01-21 17:34:14 -0600

    added variables.tf

---
+++ terraformtask/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {}
+variable "cidr" {}
+variable "azs1" {}
+variable "azs2" {}
+variable "azs3" {}
+variable "private_subnets1" {}
+variable "private_subnets2" {}
+variable "private_subnets3" {}
+variable "public_subnets1" {}
+variable "public_subnets2" {}
+variable "public_subnets3" {}
+variable "enable_nat_gateway" {}
+variable "enable_vpn_gateway" {
+  
+}
+
+
