0423daf847ee3484d56505add65b58aa8228831d
natan1988 <natan88@mail.ru>
2020-01-22 19:21:19 -0600

    removed spaces

--- user/backend.tf
+++ user/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "test-maksat-for-interview-us-east1 "
+    bucket = "test-maksat-for-interview-us-east1"
     key    = "users"
     region = "us-east-1"
   }
